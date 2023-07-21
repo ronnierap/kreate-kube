7ad7df841030969c329552a9c122f0f724728420
Mark Hooijkaas <mark@kisst.org>
2023-07-21 11:39:37 +0200

    moved namespace and replicas from env to app

--- kreate/app.py
+++ kreate/app.py
@@ -4,9 +4,7 @@ from . import templates
 class Environment:
     def __init__(self, name: str):
         self.name = name
-        self.replicas = 1
         self.vars = dict()
-        self.namespace = "demo-" + self.name
 
     def add_var(self, name: str, value: str) -> None:
         self.vars[name] = value
@@ -18,8 +16,10 @@ class App:
         self.name = name
         self.version = version
         self.env = env
+        self.namespace = self.name + "-" + self.env.name
         self.image_name = image_name or name + ".app"
         self.image_repo = "somewhere.todo/"
         self.labels = dict()
         self.target_dir = "./build/target"
         self.template_package = template_package
+        self.replicas=1
