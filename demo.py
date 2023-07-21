b72f1dfa6c61dbd2faf20231e4b2b19d860ff7aa
Mark Hooijkaas <mark@kisst.org>
2023-07-21 11:03:44 +0200

    fix labels

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ env = kreate.Environment('acc')
 env.project = "kreate-test"
 
 app = kreate.App('demo', 'v1.2', env)
-app.labels["egress-to-oracle"] = "enabled"
+#app.labels["egress-to-oracle"] = "enabled"
 
 
 kust = kreate.Kustomization(app)
@@ -16,6 +16,8 @@ ingr.whitelist("ggg")
 ingr.basic_auth()
 
 depl = kust.add(kreate.Deployment(app))
+depl.yaml.spec.template.metadata.labels.add("egress-to-oracle", "enabled")
+
 pdb = kust.add(kreate.PodDisruptionBudget(app))
 pdb.yaml.spec.minAvailable = 2
 
