aa0f1b30fd44c802dc0e4c8cea56c35dbe81459d
Mark Hooijkaas <mark@kisst.org>
2023-07-24 19:23:25 +0200

    some cleanup

--- test/demo.py
+++ test/demo.py
@@ -11,6 +11,7 @@ def demo_app():
     ingr.whitelist("ggg")
     ingr.basic_auth()
     ingr.add_label("dummy", "jan")
+    ingr = kreate.Ingress(app, path="/api", name="demo-api")
 
     depl = kreate.Deployment(app)
     depl.add_template_label("egress-to-oracle", "enabled")
@@ -23,6 +24,7 @@ def demo_app():
     cm.add_var("ORACLE_URL")
     cm.add_var("ORACLE_USR")
     cm.add_var("ORACLE_SCHEMA")
+
 
     kust = kreate.Kustomization(app)
     #kust.add_cm(cm)
