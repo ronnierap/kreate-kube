dc98f4665956310203c1f6696227582719b7ebdb
Mark Hooijkaas <mark@kisst.org>
2023-07-28 13:55:55 +0200

    simnple tests for DeepChain map

--- tests/test-merge.py
+++ tests/test-merge.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import kreate.yaml
-from kreate.wrapper import wrap
+from kreate.wrapper import wrap, DeepChain
 import copy
+from collections import ChainMap
+from collections.abc import Mapping
 
 a={
     "a": 1,
@@ -13,7 +15,6 @@ a={
 
 
 b={
-    "a": 1,
     "b": {
         "b2": 22,
         "b3": 3
@@ -21,15 +22,31 @@ b={
     "c": 33
 }
 
-
 wa=wrap(copy.deepcopy(a))
 wb=wrap(b)
 
-print(wb.c)
+#print(wa)
+#print(wb)
+c=DeepChain(wb,wa)
+
+print(f"a={c.a}")
+print(f"b={c.b}")
+print(f"b.b1={c.b.b1}")
+print(f"b.b2={c.b.b2}")
+print(f"b.b3={c.b.b3}")
+print(f"len(b)={len(c.b)}")
+for k in c.b.keys():
+    print(k)
+
+print("\n\n\n")
+
+print (a)
+print (b)
 kreate.yaml.merge(a,b)
 print (a)
+print("\n\n\n")
 
-
-kreate.yaml.merge(wa,wb)
 print (wa)
-print(a)
+print (wb)
+kreate.yaml.merge(wa,wb)
+print (wb)
