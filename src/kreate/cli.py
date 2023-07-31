490111c5de017f87057b586b0cb7ec926770e436
Mark Hooijkaas <mark@kisst.org>
2023-08-01 01:22:56 +0200

    added testupdate command

--- src/kreate/cli.py
+++ src/kreate/cli.py
@@ -33,6 +33,12 @@ def do_test(kreate_app, env):
     print(cmd)
     os.system(cmd)
 
+def do_testupdate(kreate_app, env):
+    app=kreate_app(env)
+    app.kreate_files()
+    cmd = f"kustomize build {app.target_dir} > {app.script_dir}/test.out"
+    print(cmd)
+    os.system(cmd)
 
 
 def run_cli(kreate_app):
@@ -49,11 +55,13 @@ def run_cli(kreate_app):
     apply_cmd = subparsers.add_parser("apply", help="apply the output to kubernetes", aliases=["a"])
     diff_cmd = subparsers.add_parser("diff", help="diff with current existing resources", aliases=["d"])
     test_cmd = subparsers.add_parser("test", help="test output against test.out file", aliases=["t"])
+    testupdate_cmd = subparsers.add_parser("testupdate", help="update test.out file", aliases=["tu"])
     files_cmd.set_defaults(func=do_files)
     out_cmd.set_defaults(func=do_out)
     diff_cmd.set_defaults(func=do_diff)
     apply_cmd.set_defaults(func=do_apply)
     test_cmd.set_defaults(func=do_test)
+    testupdate_cmd.set_defaults(func=do_testupdate)
     # from https://stackoverflow.com/questions/6365601/default-sub-command-or-handling-no-sub-command-with-argparse
     parser.set_defaults(func=do_files) # TODO: better way to set default command?
 
