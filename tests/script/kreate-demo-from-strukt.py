e94bc2551dd9f71a078dc9793f36bf337113b898
Mark Hooijkaas <mark@kisst.org>
2023-08-06 16:03:36 +0200

    ckeanup of test files

--- tests/script/kreate-demo-from-strukt.py
+++ tests/script/kreate-demo-from-strukt.py
@@ -3,15 +3,8 @@ import kreate
 
 def kreate_appdef(appdef:str, env: str) -> kreate.AppDef:
     # ignore passed in appdef
-    appdef = kreate.AppDef(env, "tests/script/appdef-extras.yaml")
+    appdef = kreate.AppDef(env, "tests/script/appdef.yaml")
     #appdef.app_class = kreate.KustApp
     return appdef
-
-
-#def kreate_app(appdef:str, env: str) -> kreate.App:
-#    app_cfg = kreate_config(appdef, env)
-#    app = kreate.KustApp(app_cfg, env)
-#    app.kreate_from_config()
-#    return app
 
 kreate.run_cli(kreate_appdef)
