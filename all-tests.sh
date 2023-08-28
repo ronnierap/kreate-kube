f96bce846f37f847985328f79260d3aa0b970120
Mark Hooijkaas <mark@kisst.org>
2023-08-28 18:28:47 +0200

    fixed some small bugs

--- all-tests.sh
+++ all-tests.sh
@@ -5,12 +5,12 @@ export KREATE_KRYPT_KEY_DEV=C6XOvZALFPjTzWKOPV3EJFIpmmwMhXEE
 
 echo '###########################################'
 echo python3 -m kreate.kube -a tests/demo/konfig.yaml -w test
-python3 -m kreate.kube -a tests/demo/konfig.yaml -w test
+python3 -m kreate.kube --konf tests/demo/konfig.yaml -w test
 
 echo '###########################################'
 echo ./tests/demo/kreate-demo-from-script.py -a ./tests/demo -w test
-./tests/demo/kreate-demo-from-script.py -a ./tests/demo -w test
+./tests/demo/kreate-demo-from-script.py --konf ./tests/demo -w test
 
 echo '###########################################'
 echo ./tests/demo/kreate-demo-from-strukt.py -a ./tests/demo -w test
-./tests/demo/kreate-demo-from-strukt.py -a ./tests/demo  -w test
+./tests/demo/kreate-demo-from-strukt.py --konf ./tests/demo  -w test
