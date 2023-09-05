f46b8a02d31d09a4094909dc99b8c556e2151f97
Mark Hooijkaas <mark@kisst.org>
2023-09-05 09:04:31 +0200

    surpress warnings for missing kreate-kube package

--- all-tests.sh
+++ all-tests.sh
@@ -4,13 +4,13 @@ export KREATE_KRYPT_KEY_DEV=C6XOvZALFPjTzWKOPV3EJFIpmmwMhXEE
 
 
 echo '###########################################'
-echo python3 -m kreate.kube --konf tests/demo/konfig.yaml -w test
-python3 -m kreate.kube --konf tests/demo/konfig.yaml -w test
+echo python3 -m kreate.kube --konf tests/demo/konfig.yaml -w -R test
+python3 -m kreate.kube --konf tests/demo/konfig.yaml -w -R test
 
 echo '###########################################'
-echo ./tests/demo/kreate-demo-from-script.py --konf ./tests/demo -w test
-./tests/demo/kreate-demo-from-script.py --konf ./tests/demo -w test
+echo ./tests/demo/kreate-demo-from-script.py --konf ./tests/demo -w -R test
+./tests/demo/kreate-demo-from-script.py --konf ./tests/demo -w -R test
 
 echo '###########################################'
-echo ./tests/demo/kreate-demo-from-strukt.py --konf ./tests/demo -w test
-./tests/demo/kreate-demo-from-strukt.py --konf ./tests/demo  -w test
+echo ./tests/demo/kreate-demo-from-strukt.py --konf ./tests/demo -w -R test
+./tests/demo/kreate-demo-from-strukt.py --konf ./tests/demo  -w -R test
